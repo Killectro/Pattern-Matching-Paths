474b6096d3ac56e857e61f60290f85b33d844165
Killectro <killectro@gmail.com>
2016-05-20 00:16:23 -0400

    Refactor to pull out STDIO reading into separate struct and add Pattern sorting function

--- Pattern.swift
+++ Pattern.swift
@@ -22,3 +22,29 @@ struct Pattern {
         numberOfWildcards = components.filter{$0 == Pattern.WILDCARD}.count
     }
 }
+
+// Custom sort operator for Patterns
+func < (lhs: Pattern, rhs: Pattern) -> Bool {
+    if lhs.numberOfWildcards == rhs.numberOfWildcards {
+        
+        for (index, leftComponent) in lhs.components.enumerate() {
+            let rightComponent = rhs.components[index]
+            
+            if leftComponent == Pattern.WILDCARD && rightComponent == Pattern.WILDCARD {
+                // They are the both wildcards, keep searching
+                continue
+            } else if leftComponent == Pattern.WILDCARD {
+                // Left has a wildcard but right doesn't, right should go first
+                return false
+            } else if rightComponent == Pattern.WILDCARD {
+                // Right has a wildcard but left doesn't, left should go first
+                return true
+            }
+        }
+        
+        // This will only exectute if two patterns are exactly the same
+        return true
+    } else {
+        return lhs.numberOfWildcards < rhs.numberOfWildcards
+    }
+}
--- main.swift
+++ main.swift
@@ -14,40 +14,6 @@ func writeToStdErr(str: String) {
 // MARK: - Script entry point
 
 // Read patterns from stdin and put into memory
-guard let nString = readLine(), N = Int(nString) else {
-    writeToStdErr("Invalid input format: The first line of the file must be a number")
-    exit(EXIT_FAILURE)
-}
-
-var patterns = [Pattern]()
-var index = 0
-
-while index < N {
-    guard let pattern = readLine() else {
-        writeToStdErr("Invalid input format: Reached EOF before expected")
-        exit(EXIT_FAILURE)
-    }
-    
-    patterns.append(Pattern(fromString: pattern))
-    index += 1
-}
-
-// Read paths from stdin and put into memory
-guard let mString = readLine(), M = Int(mString) else {
-    writeToStdErr("Invalid input format: The next line of the file must be a number")
-    exit(EXIT_FAILURE)
-}
-
-var paths = [Path]()
-index = 0
-while index < M {
-    guard let path = readLine() else {
-        writeToStdErr("Invalid input format: Reached EOF before expected")
-        exit(EXIT_FAILURE)
-    }
-    
-    paths.append(Path(fromString: path))
-    index += 1
-}
+let (patterns, paths) = InputReader.getPatternsAndPathsFromStandardInput()
 
 print(patterns.map{$0.components})
