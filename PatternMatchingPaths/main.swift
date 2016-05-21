eedd2df58827154a815bcbb0bfa6dab5b82a1ba2
Killectro <killectro@gmail.com>
2016-05-21 13:23:53 -0400

    Add better comment to explain sorting

--- PatternMatchingPaths/main.swift
+++ PatternMatchingPaths/main.swift
@@ -7,7 +7,8 @@ import Foundation
 // Read patterns from stdin and put into memory
 let (patterns, paths) = InputReader.getPatternsAndPathsFromStandardInput()
 
-// Sort the patterns in order of "bestness" defined in problem requirements
+// Sort the patterns in descending order of "bestness" defined in problem requirements
+// See description of >(_:_:) method for more info
 let sortedPatterns = patterns.sort(>)
 
 for path in paths {
